b3861fba08590b47b97a16bb1448aecd58bc2a91
davidZahedi <davidza001@gmail.com>
2018-07-31 10:34:54 +0200

    test the last model

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-saved_model = 'rotated_21_epoch.h5'
+saved_model = 'rotated_15_21_epoch.h5'
 err = []
 model = load_model(saved_model)
 p = model.predict(x_test.reshape(10000,28,28,1))
